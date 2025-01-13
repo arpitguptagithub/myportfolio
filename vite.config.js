00753671088ea9372c138914163671da69a75682
arpit <arpit@sqrx.com>
2025-01-13 18:49:18 +0530

    all

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/myportfolio/', // Replace with your actual repository name
+});
